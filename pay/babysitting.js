ec3183203d452aba7c5e048ebd18cda9c65b3d69
Benjamin Soyka <bensoyka@icloud.com>
2020-02-17 11:10:46 -0700

    Begin full SoykaPay switch to Firebase

--- pay/babysitting.js
+++ pay/babysitting.js
@@ -1,89 +1,80 @@
 var records, intervalId, hours, minutes, stopwatchContent, benEarnings, mattEarnings;
 var babysittingDiv = document.getElementById("babysitting");
-instance({
-    method: "get",
-    url: "/Babysitting"
-}).then(function (response) {
-    records = response.data.records;
-    if (!records || records.length === 0) {
-        // ********************************
-        // NO RECORDS --- SHOW START SCREEN
-        // ********************************
-        var babysittingStartButton = document.createElement("button");
-        babysittingStartButton.innerHTML = "Start Stopwatch";
-        babysittingStartButton.setAttribute("onclick", "startBabysittingStopwatch();");
-        babysittingStartButton.setAttribute("class", "permBabysit btn btn-outline-primary");
-        babysittingDiv.appendChild(babysittingStartButton);
-    } else {
-        // ************************************************
-        // RECORD EXISTS --- SHOW STOPWATCH AND STOP SCREEN
-        // ************************************************
-        var startedAtDiv = document.createElement("div");
-        startedAtDiv.setAttribute("id", "babysittingTimes");
-        startedAtDiv.innerHTML = "<b>Started:</b> " + (new Date(records[0].fields["Start Time"]));
-        babysittingDiv.appendChild(startedAtDiv);
-        babysittingDiv.appendChild(document.createElement("br"));
-        var stopwatchDiv = document.createElement("div");
-        stopwatchDiv.setAttribute("id", "stopwatch");
-        babysittingDiv.appendChild(stopwatchDiv);
-        var stopwatchEndButton = document.createElement("button");
-        stopwatchEndButton.setAttribute("onclick", "endBabysittingStopwatch();");
-        stopwatchEndButton.setAttribute("id", "stopwatchEndButton");
-        stopwatchEndButton.setAttribute("class", "btn btn-outline-primary");
-        stopwatchEndButton.innerHTML = "End Stopwatch";
-        babysittingDiv.appendChild(document.createElement("br"));
-        babysittingDiv.appendChild(stopwatchEndButton);
-        var earningsDiv = document.createElement("div");
-        babysittingDiv.appendChild(earningsDiv);
-        intervalId = setInterval(function () {
-            var milliseconds = parseInt((new Date()) - (new Date(records[0].fields["Start Time"])));
-            minutes = Math.floor(milliseconds / 60000);
-            stopwatchContent = minutes + "min";
-            if (minutes >= 60) {
-                // Need to include hours
-                hours = Math.floor(minutes / 60);
-                minutes = minutes % 60;
-                if (minutes != hours * 60) {
-                    stopwatchContent = hours + "hr " + minutes + "min";
-                    if (hours > 1) {
-                        stopwatchContent = hours + "hrs " + minutes + "min";
+
+function reloadBabysitting() {
+    db.collection("babysitting").get().then(function (response) {
+        babysittingDiv.innerHTML = "";
+        records = response.docs;
+        console.log(records);
+        if (records.length == 0) {
+            // ********************************
+            // NO RECORDS --- SHOW START SCREEN
+            // ********************************
+            var babysittingStartButton = document.createElement("button");
+            babysittingStartButton.innerHTML = "Start Stopwatch";
+            babysittingStartButton.setAttribute("onclick", "startBabysittingStopwatch();");
+            babysittingStartButton.setAttribute("class", "permBabysit btn btn-outline-primary");
+            babysittingDiv.appendChild(babysittingStartButton);
+        } else {
+            // ************************************************
+            // RECORD EXISTS --- SHOW STOPWATCH AND STOP SCREEN
+            // ************************************************
+            var startedAtDiv = document.createElement("div");
+            startedAtDiv.setAttribute("id", "babysittingTimes");
+            startedAtDiv.innerHTML = "<b>Started:</b> " + (records[0].data().started.toDate());
+            babysittingDiv.appendChild(startedAtDiv);
+            babysittingDiv.appendChild(document.createElement("br"));
+            var stopwatchDiv = document.createElement("div");
+            stopwatchDiv.setAttribute("id", "stopwatch");
+            babysittingDiv.appendChild(stopwatchDiv);
+            var stopwatchEndButton = document.createElement("button");
+            stopwatchEndButton.setAttribute("onclick", "endBabysittingStopwatch();");
+            stopwatchEndButton.setAttribute("id", "stopwatchEndButton");
+            stopwatchEndButton.setAttribute("class", "btn btn-outline-primary");
+            stopwatchEndButton.innerHTML = "End Stopwatch";
+            babysittingDiv.appendChild(document.createElement("br"));
+            babysittingDiv.appendChild(stopwatchEndButton);
+            var earningsDiv = document.createElement("div");
+            babysittingDiv.appendChild(earningsDiv);
+            intervalId = setInterval(function () {
+                var milliseconds = parseInt((new Date()) - (records[0].data().started.toDate()));
+                minutes = Math.floor(milliseconds / 60000);
+                stopwatchContent = minutes + "min";
+                if (minutes >= 60) {
+                    // Need to include hours
+                    hours = Math.floor(minutes / 60);
+                    minutes = minutes % 60;
+                    if (minutes != hours * 60) {
+                        stopwatchContent = hours + "hr " + minutes + "min";
+                        if (hours > 1) {
+                            stopwatchContent = hours + "hrs " + minutes + "min";
+                        }
+                    } else {
+                        stopwatchContent = hours + "hr";
+                        if (hours > 1) {
+                            stopwatchContent = hours + "hrs";
+                        }
                     }
                 } else {
-                    stopwatchContent = hours + "hr";
-                    if (hours > 1) {
-                        stopwatchContent = hours + "hrs";
-                    }
+                    hours = 0;
                 }
-            } else {
-                hours = 0;
-            }
-            stopwatchDiv.innerHTML = "<b>Time Elapsed</b>: " + stopwatchContent;
-            // Calculate earnings per person
-            var totalHours = hours + (minutes / 60);
-            benEarnings = (totalHours * 5).toLocaleString(undefined, {
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
-            });
-            mattEarnings = (totalHours * 1).toLocaleString(undefined, {
-                minimumFractionDigits: 2,
-                maximumFractionDigits: 2
-            });
-            earningsDiv.innerHTML = "<b>Ben's Earnings</b>: $" + benEarnings + "<br><br><b>Matt's Earnings</b>: $" + mattEarnings;
-        }, 1000);
-    }
-});
+                stopwatchDiv.innerHTML = "<b>Time Elapsed</b>: " + stopwatchContent;
+                // Calculate earnings per person
+                var totalHours = hours + (minutes / 60);
+                benEarnings = (totalHours * 5).toFixed(2);
+                mattEarnings = (totalHours * 1).toFixed(2);
+                earningsDiv.innerHTML = "<b>Ben's Earnings</b>: $" + benEarnings + "<br><br><b>Matt's Earnings</b>: $" + mattEarnings;
+            }, 1000);
+        }
+    });
+}
+reloadBabysitting();
 
 function startBabysittingStopwatch() {
-    instance({
-        method: "post",
-        url: "/Babysitting",
-        data: {
-            "fields": {
-                "Start Time": (new Date()).toISOString()
-            }
-        }
+    db.collection("babysitting").add({
+        started: firebase.firestore.Timestamp.fromDate(new Date())
     }).then(function (response) {
-        location.reload();
+        reloadBabysitting();
     });
 }
 
@@ -97,15 +88,9 @@ function endBabysittingStopwatch() {
     // Remove stopwatch end button
     document.getElementById("stopwatchEndButton").parentElement.removeChild(document.getElementById("stopwatchEndButton"));
     // Get AIrtable record list
-    instance({
-        method: "get",
-        url: "/Babysitting"
-    }).then(function (response) {
+    db.collection("babysitting").get().then(function (response) {
         // Remove first and only record
-        instance({
-            method: "delete",
-            url: "/Babysitting/" + response.data.records[0].id
-        });
+        db.collection("babysitting").doc(response.docs[0].id).delete();
     });
     // Calculate earnings per person
     var totalHours = hours + (minutes / 60);
@@ -123,48 +108,13 @@ function endBabysittingStopwatch() {
 }
 
 function addEarnings() {
-    function getBenBalance() {
-        return instance({
-            method: "get",
-            url: "/IOU/reczNIYdM2ZGEmWEM"
+    db.collection("users").doc("jQmILFjXMVNUHpVBlJ420c3aNp43").update({
+        balance: firebase.firestore.FieldValue.increment(parseFloat(benEarnings))
+    }).then(function () {
+        db.collection("users").doc("j3CMDV2x1UUPERcsPs4nIXOkZMD2").update({
+            balance: firebase.firestore.FieldValue.increment(parseFloat(mattEarnings))
+        }).then(function () {
+            reloadBabysitting();
         });
-    }
-
-    function getMattBalance() {
-        return instance({
-            method: "get",
-            url: "/IOU/recjcKmBpxMZtgDrW"
-        });
-    }
-    axios.all([getBenBalance(), getMattBalance()]).then(axios.spread(function (benBalanceResponse, mattBalanceResponse) {
-        var newBenBalance = parseFloat(benBalanceResponse.data.fields.Balance) + parseFloat(benEarnings);
-        var newMattBalance = parseFloat(mattBalanceResponse.data.fields.Balance) + parseFloat(mattEarnings);
-
-        function updateBenBalance() {
-            return instance({
-                method: "patch",
-                url: "/IOU/reczNIYdM2ZGEmWEM",
-                data: {
-                    "fields": {
-                        "Balance": newBenBalance
-                    }
-                }
-            });
-        }
-
-        function updateMattBalance() {
-            return instance({
-                method: "patch",
-                url: "/IOU/recjcKmBpxMZtgDrW",
-                data: {
-                    "fields": {
-                        "Balance": newMattBalance
-                    }
-                }
-            });
-        }
-        axios.all([updateBenBalance(), updateMattBalance()]).then(axios.spread(function (benBalanceUpdateResponse, mattBalanceUpdateResponse) {
-            location.reload();
-        }));
-    }));
+    });
 }
--- pay/iou.js
+++ pay/iou.js
@@ -1,91 +1,76 @@
 var currentBalances = {};
 var table = document.getElementById("currentBalances");
 
-function bodyLoaded() {
-    instance({
-        method: "get",
-        url: "/IOU"
-    }).then(function (response) {
-        response.data.records.forEach(function (record) {
-            currentBalances[record.fields.Relationship] = {
-                balance: record.fields.Balance,
-                id: record.id,
-                quickActions: JSON.parse(record.fields["Quick Actions"])
-            };
-        });
-        for (var relationship in currentBalances) {
-            var relationshipBalanceElement = document.createElement("tr");
-            var relationshipDisplayElement = document.createElement("th");
-            relationshipDisplayElement.setAttribute("scope", "row");
-            relationshipDisplayElement.innerHTML = relationship;
-            relationshipBalanceElement.appendChild(relationshipDisplayElement);
-            var balanceDisplayElement = document.createElement("td");
-            balanceDisplayElement.innerHTML = "$" + currentBalances[relationship].balance.toLocaleString(undefined, {
-	            minimumFractionDigits: 2
-	        });
-            relationshipBalanceElement.appendChild(balanceDisplayElement);
-            var updateElement = document.createElement("td");
-            var updateInput = document.createElement("input");
-            var updateInputGroup = document.createElement("div");
-            updateInputGroup.setAttribute("class", "input-group");
-            updateInput.setAttribute("size", "5");
-            updateInput.setAttribute("class", "form-control");
-            updateInput.setAttribute("id", "UPDATEINPUT" + currentBalances[relationship].id);
-            updateElement.setAttribute("class", "permEdit");
-            updateElement.appendChild(updateInputGroup);
-            updateInputGroup.appendChild(updateInput)
-            var updateSpacingElement = document.createElement("span");
-            updateSpacingElement.innerHTML = "&nbsp;";
-            updateElement.appendChild(updateSpacingElement);
-            var updateButton = document.createElement("button");
-            updateButton.innerHTML = "Update";
-            updateButton.setAttribute("class", "btn btn-outline-primary");
-            updateButton.setAttribute("onclick", "updateDatabase('UPDATEINPUT" + currentBalances[relationship].id + "', document.getElementById('UPDATEINPUT" + currentBalances[relationship].id + "').value);");
-            var updateInputGroupAppend = document.createElement("div");
-            updateInputGroupAppend.setAttribute("class", "input-group-append");
-            updateInputGroupAppend.appendChild(updateButton);
-            updateInputGroup.appendChild(updateInputGroupAppend);
-            relationshipBalanceElement.appendChild(updateElement);
-            // --------------------------------------------------------
-            var quickUpdateElement = document.createElement("td");
-            quickUpdateElement.setAttribute("class", "permEdit");
-            var quickUpdateElementGroup = document.createElement("div");
-            quickUpdateElementGroup.setAttribute("class", "btn-group");
-            quickUpdateElementGroup.setAttribute("role", "group");
-            quickUpdateElement.appendChild(quickUpdateElementGroup);
-            var quickUpdateSpacingElement = document.createElement("span");
-            quickUpdateSpacingElement.innerHTML = "&nbsp;";
-            quickUpdateElement.appendChild(quickUpdateSpacingElement);
-            for (var quickTitle in currentBalances[relationship].quickActions) {
-                var quickUpdateButton = document.createElement("button");
-                quickUpdateButton.innerHTML = quickTitle;
-                quickUpdateButton.setAttribute("class", "btn btn-outline-primary");
-                quickUpdateButton.setAttribute("onclick", "updateDatabase('UPDATEINPUT" + currentBalances[relationship].id + "', " + currentBalances[relationship].quickActions[quickTitle] + ");");
-                quickUpdateElementGroup.appendChild(quickUpdateButton);
-            }
-            relationshipBalanceElement.appendChild(quickUpdateElement);
-            // --------------------------------------------------------
-            relationshipBalanceElement.setAttribute("data-record-id", currentBalances[relationship].id);
-            relationshipBalanceElement.setAttribute("data-record-balance", currentBalances[relationship].balance);
-            table.getElementsByTagName("tbody")[0].appendChild(relationshipBalanceElement);
-        }
+db.collection("users").where("kid", "==", true).onSnapshot(function (response) {
+    table.getElementsByTagName("tbody")[0].innerHTML = "";
+    response.forEach(function (doc) {
+        console.log(doc.data());
+        currentBalances[doc.data().name] = {
+            balance: doc.data().balance,
+            id: doc.id,
+            quickActions: doc.data().actions
+        };
     });
-}
+    for (var relationship in currentBalances) {
+        var relationshipBalanceElement = document.createElement("tr");
+        var relationshipDisplayElement = document.createElement("th");
+        relationshipDisplayElement.setAttribute("scope", "row");
+        relationshipDisplayElement.innerHTML = relationship;
+        relationshipBalanceElement.appendChild(relationshipDisplayElement);
+        var balanceDisplayElement = document.createElement("td");
+        balanceDisplayElement.innerHTML = "$" + currentBalances[relationship].balance.toFixed(2);
+        relationshipBalanceElement.appendChild(balanceDisplayElement);
+        var updateElement = document.createElement("td");
+        var updateInput = document.createElement("input");
+        var updateInputGroup = document.createElement("div");
+        updateInputGroup.setAttribute("class", "input-group");
+        updateInput.setAttribute("size", "5");
+        updateInput.setAttribute("class", "form-control");
+        updateInput.setAttribute("id", "UPDATEINPUT" + currentBalances[relationship].id);
+        updateElement.setAttribute("class", "permEdit");
+        updateElement.appendChild(updateInputGroup);
+        updateInputGroup.appendChild(updateInput)
+        var updateSpacingElement = document.createElement("span");
+        updateSpacingElement.innerHTML = "&nbsp;";
+        updateElement.appendChild(updateSpacingElement);
+        var updateButton = document.createElement("button");
+        updateButton.innerHTML = "Update";
+        updateButton.setAttribute("class", "btn btn-outline-primary");
+        updateButton.setAttribute("onclick", "updateDatabase('UPDATEINPUT" + currentBalances[relationship].id + "', document.getElementById('UPDATEINPUT" + currentBalances[relationship].id + "').value);");
+        var updateInputGroupAppend = document.createElement("div");
+        updateInputGroupAppend.setAttribute("class", "input-group-append");
+        updateInputGroupAppend.appendChild(updateButton);
+        updateInputGroup.appendChild(updateInputGroupAppend);
+        relationshipBalanceElement.appendChild(updateElement);
+        // --------------------------------------------------------
+        var quickUpdateElement = document.createElement("td");
+        quickUpdateElement.setAttribute("class", "permEdit");
+        var quickUpdateElementGroup = document.createElement("div");
+        quickUpdateElementGroup.setAttribute("class", "btn-group");
+        quickUpdateElementGroup.setAttribute("role", "group");
+        quickUpdateElement.appendChild(quickUpdateElementGroup);
+        var quickUpdateSpacingElement = document.createElement("span");
+        quickUpdateSpacingElement.innerHTML = "&nbsp;";
+        quickUpdateElement.appendChild(quickUpdateSpacingElement);
+        for (var quickTitle in currentBalances[relationship].quickActions) {
+            var quickUpdateButton = document.createElement("button");
+            quickUpdateButton.innerHTML = quickTitle;
+            quickUpdateButton.setAttribute("class", "btn btn-outline-primary");
+            quickUpdateButton.setAttribute("onclick", "updateDatabase('UPDATEINPUT" + currentBalances[relationship].id + "', " + currentBalances[relationship].quickActions[quickTitle] + ");");
+            quickUpdateElementGroup.appendChild(quickUpdateButton);
+        }
+        relationshipBalanceElement.appendChild(quickUpdateElement);
+        // --------------------------------------------------------
+        relationshipBalanceElement.setAttribute("data-record-id", currentBalances[relationship].id);
+        relationshipBalanceElement.setAttribute("data-record-balance", currentBalances[relationship].balance);
+        table.getElementsByTagName("tbody")[0].appendChild(relationshipBalanceElement);
+    }
+});
 
 function updateDatabase(inputId, amountToAdd) {
     var inputElement = document.getElementById(inputId);
     var recordId = inputElement.parentElement.parentElement.parentElement.getAttribute("data-record-id");
-    var currentBalance = parseFloat(inputElement.parentElement.parentElement.parentElement.getAttribute("data-record-balance"));
-    var newBalance = currentBalance + parseFloat(amountToAdd);
-    instance({
-        method: "patch",
-        url: "/IOU/" + recordId,
-        data: {
-            "fields": {
-                "Balance": newBalance
-            }
-        }
-    }).then(function (response) {
-        location.reload();
+    db.collection("users").doc(recordId).update({
+        balance: firebase.firestore.FieldValue.increment(parseFloat(amountToAdd))
     });
 }
